a6ce92f51e32e82f7a787f3e293a53a2c3935295
Sarah Turek <snturek@gmail.com>
2015-10-06 22:19:04 -0600

    added mongolabs

--- server/_config.js
+++ server/_config.js
@@ -3,6 +3,7 @@ var config = {};
 config.mongoURI = {
   development: 'mongodb://localhost/scavenger-hunt',
   test: 'mongodb://localhost/scavenger-hunt-test',
+  production: 'mongodb://heroku_bt28mvcx:crdg041hs42dj6sbbbevrlb1g6@ds033143.mongolab.com:33143/heroku_bt28mvcx'
 };
 
 module.exports = config;
